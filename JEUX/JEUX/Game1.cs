dcbe3e29e21e3b759e8d7f3b54908a6b8a4d9df9
Justin Bonansea <justin.bonansea@etu.univ-smb.fr>
2023-01-04 11:44:27 +0100

    bouton et background

--- JEUX/JEUX/Game1.cs
+++ JEUX/JEUX/Game1.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using jeux.Controls;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace jeux
 {
@@ -9,6 +11,9 @@ namespace jeux
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        private Color _backgroundColour = Color.CornflowerBlue;
+        private List<Component> _gameComponents;
 
         private Texture2D _textureBackgroundMenu;
 
@@ -29,6 +34,8 @@ namespace jeux
             _graphics.PreferredBackBufferHeight = HAUTEUR_FENETRE;
             _graphics.ApplyChanges();
 
+            IsMouseVisible = true;
+
             base.Initialize();
         }
 
@@ -36,14 +43,48 @@ namespace jeux
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            var playButton = new Button(Content.Load<Texture2D>("Controls/Button3"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(200 , 250),
+                Text = "Play",
+            };
+
+            playButton.Click += PlayButton_Click;
+
+            var quitButton = new Button(Content.Load<Texture2D>("Controls/Button3"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(1000, 250),
+                Text = "Quit",
+            };
+
+            quitButton.Click += QuitButton_Click;
+
+            _gameComponents = new List<Component>()
+            {
+                playButton,
+                quitButton,
+            };
+
             _textureBackgroundMenu = Content.Load<Texture2D>("backgroundMenu");
             // TODO: use this.Content to load your game content here
         }
 
+        private void QuitButton_Click(object sender, System.EventArgs e)
+        {
+            Exit();
+        }
+
+        private void PlayButton_Click(object sender, System.EventArgs e)
+        {
+            var random = new Random();
+
+            _backgroundColour = new Color(random.Next(0, 255), random.Next(0, 255),random.Next(0, 255));
+        }
+
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            foreach(var component in _gameComponents)
+                component.Update(gameTime);
 
             // TODO: Add your update logic here
 
@@ -52,10 +93,12 @@ namespace jeux
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(_backgroundColour);
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(_textureBackgroundMenu, new Rectangle(0, 0, LARGEUR_FENETRE, HAUTEUR_FENETRE), Color.White);
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, _spriteBatch);
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
