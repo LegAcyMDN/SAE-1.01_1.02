c56e6cb8ee8185ff3280d9a78cc4d1cc433feba9
abahmanh <abahmanh@D353-S01>
2023-01-04 11:44:33 +0100

    Characters

--- JEUX/JEUX/Game1.cs
+++ JEUX/JEUX/Game1.cs
@@ -6,6 +6,29 @@ namespace jeux
 {
     public class Game1 : Game
     {
+       
+        public const int hauteurP = 150;
+         public const int largeurP = 50;
+      
+
+       
+        private Texture2D _textureCharacterP;
+        private Texture2D _texturePoint;
+        private Texture2D _textureObstacleP;
+        private Texture2D _textureObstacleD;
+        private Texture2D _textureObstacleT;
+        private Texture2D _textureObstacleQ;
+
+
+        private int score;
+        private int credit;
+        private int vie;
+        int _sensCharacter;
+
+
+        private SpriteFont _police;
+        
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -14,20 +37,39 @@ namespace jeux
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
         }
 
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        { 
+           
 
-            base.Initialize();
+            _textureCharacterP = Content.Load<Texture2D>("characterP");
+            _texturePoint = Content.Load<Texture2D>("Point");
+            _textureObstacleP = Content.Load<Texture2D>("ObstacleP");
+            _textureObstacleD = Content.Load<Texture2D>("ObstacleD");
+            _textureObstacleT = Content.Load<Texture2D>("ObstacleT");
+            _textureObstacleQ = Content.Load<Texture2D>("ObstacleQ");
+
+
+            
+
+
+            credit = 0;
+            score = 0;
+            vie = 3;
+
+
+            _graphics.ApplyChanges();
+             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,18 +77,25 @@ namespace jeux
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
 
+            _spriteBatch.DrawString(_police, $"Score  : {score}", _positionScore, Color.White);
+            _spriteBatch.DrawString(_police, $"Vie   : {vie}", _positionVie, Color.Red);
+            _spriteBatch.DrawString(_police, $"Crédit  : {credit}", _positionCredit, Color.Blue);
+
+
+
+            _spriteBatch.Begin();
             base.Draw(gameTime);
+
+
         }
     }
 }
